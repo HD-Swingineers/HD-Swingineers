c4d8f732620ea38861c326b7dbfdc6b2e8c2384f
Michael <michael.pieniazek@gmail.com>
2016-10-28 18:21:20 +1100

    test

--- tests/snake-test.js
+++ tests/snake-test.js
@@ -1,18 +1,16 @@
 QUnit.test('snake direction', function(assert) {
 	var snkRight = Direction.RIGHT;
-	var snkLeft = Direction.RIGHT;
-	var snkUp = Direction.RIGHT;
-	var snkDoWn = Direction.RIGHT;
-  //var leftup = (Direction.LEFT | Direction.UP)
+	var snkLeft = Direction.LEFT;
+	var snkUp = Direction.UP;
+	var snkDown = Direction.DOWN;
   
-  var result1 = Direction.flip(snkRight);
- // var result2 = Direction.flip(leftup);
-  
-  assert.equal(result1, Direction.LEFT);
-  // assert.equal(result2, Direction.RIGHT | Direction.DOWN);
-	// var snakeRight = Direction.right;  
- // 	var result = Direction.flip(right);
-  
- //  assert.equal(result, Direction.LEFT);
+	var result1 = Direction.flip(snkRight);
+	var result2 = Direction.flip(snkLeft);
+	var result3 = Direction.flip(snkUp);
+	var result4 = Direction.flip(snkDown);
 
+	assert.equal(result1, Direction.LEFT);
+	assert.equal(result2, Direction.RIGHT);
+	assert.equal(result3, Direction.DOWN);
+	assert.equal(result4, Direction.UP);
 });
