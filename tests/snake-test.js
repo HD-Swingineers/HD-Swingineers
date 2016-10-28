d216e38da97de6d37a9f1b517c487ffdda5e13fa
Michael <michael.pieniazek@gmail.com>
2016-10-28 17:28:15 +1100

    tests

---
+++ tests/snake-test.js
@@ -0,0 +1,10 @@
+QUnit.test('snake collsion', function(assert) {
+	var xPos = 0;
+	var yPos = 0;
+	var snake = [];
+
+	for (var i = 3; i>=0; i--) {
+          snake.push({x:i, y:0});
+  	}  
+	assert.eqaul(true,checkCollision(xPos, yPos, snake));
+});
